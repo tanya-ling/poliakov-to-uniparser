705568529a123ccc20372f6491c88c0e0f0a0cd3
tanya-ling <tanya96gavrilova@yandex.ru>
2017-11-01 13:20:06 +0300

    ago-ovo-ogo

--- add_ogo_ovo.py
+++ add_ogo_ovo.py
@@ -1,1 +1,24 @@
+import codecs
 
+def line_healing(line):
+    content = line.rstrip()[8:].split('//')
+    to_add = []
+    for inflex in content:
+        if inflex[-3:] == 'аго':
+            to_add.append(inflex[:-3] + 'ово')
+            to_add.append(inflex[:-3] + 'ого')
+    content += to_add
+    content = set(content)
+    return ' -flex: ' + '//'.join(content) + '\r\n'
+
+
+f = codecs.open('C:\\Users\\Tatiana\Documents\GitHub\paradigm_tagging\прилфлексии.txt', 'r', 'utf-8')
+
+a = codecs.open('C:\\Users\Tatiana\Documents\GitHub\paradigm_tagging\прилфлексии011117.txt', 'w', 'utf-8')
+
+for line in f:
+    if 'flex' in line:
+        line = line_healing(line)
+    a.write(line)
+
+a.close()
--- joinparadigms.py
+++ joinparadigms.py
@@ -3,15 +3,18 @@ import codecs
 
 def combain_paradigms():
     fn = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\paradigm_tagging\\сущфлексии.txt', 'r', 'utf-8')
-    fa = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\paradigm_tagging\\прилфлексии.txt', 'r', 'utf-8')
+    fa = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\paradigm_tagging\\прилфлексии011117.txt', 'r', 'utf-8')
     fv = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\poliakov-to-uniparser\\флексии\\глагдляпарсера.txt', 'r', 'utf-8')
     fp = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\poliakov-to-uniparser\\флексии\\all_pron_n.txt', 'r', 'utf-8')
+    fb = codecs.open(u'C:\\Users\\Tatiana\\Documents\\GitHub\\poliakov-to-uniparser\\флексии\\byti.txt', 'r', 'utf-8')
     fnr = fn.read()
     far = fa.read()
     fvr = fv.read()
     fpr = fp.read()
-    fcr = fnr + far + fvr + fpr + '\r\n-paradigm: unchangeable\r\n -flex: .\r\n  gramm: \r\n'
+    fbr = fb.read()
+    fcr = fnr + far + fvr + fpr + fbr + '\r\n-paradigm: unchangeable\r\n -flex: .\r\n  gramm: \r\n'
     fc = codecs.open('conjoined_paradigms.txt', 'w', 'utf-8')
     fc.write(fcr.replace('﻿', ''))
+    fc.close()
 
 combain_paradigms()
